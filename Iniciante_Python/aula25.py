8f3f7fddcf580db198267f710e694ea9b8d054a0
Rafael <rsantosmartins@icloud.com>
2024-03-06 15:54:22 -0300

    interpolação de string com % , formatação de strings com f-strings, função len e exercicio

---
+++ Iniciante_Python/aula25.py
@@ -0,0 +1,14 @@
+'''
+Interpolação básica de strings
+S - string
+d e i - init
+f - float
+x eX = Hexadecimal (ABCDEF0123456789)
+'''
+
+nome = 'Rafael'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
+
